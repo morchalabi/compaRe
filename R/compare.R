03bd7a6b3663f4446d38a8e77b44330e17b1963a
Morteza <mor.chalabi@gmail.com>
2021-03-22 14:14:13 +0100

    change in dbscan
    sort is not supported in lof anymore!

--- R/compare.R
+++ R/compare.R
@@ -105,7 +105,7 @@ compare = function(smpl1 = NULL, smpl2 = NULL, n_ = 5, par_ = TRUE)
       exlv_pnts = smpls_[[i_]][exlv_rgns_inds,,drop = F]      # points in exclusive regions
       if(20 <= nrow(exlv_pnts))                               # perform LOF if there are at least 20 exclusive points
       {
-        lof_ = dbscan::lof(x = exlv_pnts, k = 3, sort = F, approx = round(nrow(exlv_pnts)/1e5,1))     # kNN works too slowly if dataset size is over 1e5. approx helps to speed up
+        lof_ = dbscan::lof(x = exlv_pnts, k = 3, approx = round(nrow(exlv_pnts)/1e5,1))     # kNN works too slowly if dataset size is over 1e5. approx helps to speed up
         exlv_rgns_disim = length(unique(exlv_rgns[which(lof_ <= 1.2)]))                               # dissim in exclusive regions: for each signal exclusive region (i.e. lof <= 1.2), 1 is incremented as penalty
       }
 
